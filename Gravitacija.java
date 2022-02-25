195c511a047116443d115c55290b500493443f45
KompAntonija <ak7046@student.uni-lj.si>
2022-02-25 17:48:56 +0100

    dodajanje izpisa

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,12 @@
+import java.util.* ;
+
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
+    }
+
+    public static double pospesek(double nadmorskaV ){
+        System.out.println("Nadmorska višina:"nadmorskaV "m");
+        System.out.println("Pospešek:"pospesek"m/sˇ2");
     }
 }
