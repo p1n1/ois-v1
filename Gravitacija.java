dfc3e044eab8f5f2a8900849885374f840231fe1
kayleotp <sg5820@student.uni-lj.si>
2022-02-25 17:42:15 +0100

    dodajanje funkcije za izracun pospeska

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,18 @@
+import java.util.*;
+
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
+    }
+
+    public static double pospesek(double nadmorskaV){
+        double a = 0;
+        double c = 6.674 * Math.pow(10, -11);
+        double m = 5.972 * Math.pow(10, 24);
+        double r = 6.371 * Math.pow(10, 6);
+        
+        a = (c*m)/((r + nadmorskaV) * (r + nadmorskaV));
+
+        return a;
     }
 }
