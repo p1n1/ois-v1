68fc14d0b4bfc9b19e1e0caef45a2c471bc2b14c
Gregor Pogacar <gp9162@student.uni-lj.si>
2022-02-25 17:51:22 +0100

    Dodajanje Gravitacija.java

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+
 public class Gravitacija {
     public static void main(String[] args) {
         System.out.println("OIS je zakon!");
     }
+
+    Scanner sc = new Scanner(System.in);
+
+    double h = sc.nextInt();
+
 }
